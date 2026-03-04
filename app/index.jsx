13c906fecb91a834eb7d390369126ca41c684b26
LauraHTML <laura.senai03@gmail.com>
2026-03-03 21:32:44 -0300

    Estilização inicial

--- app/index.jsx
+++ app/index.jsx
@@ -1,15 +1,39 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet, Image } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={estilos.container}>
+      <Image style={estilos.imagem} source={require("./tomate.jpg")} />
+      <View style={estilos.actions}>
+      <Text style={estilos.timer}>25:00</Text>
+      </View>
     </View>
   );
 }
+
+const estilos = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#2b1108",
+    gap: 40,
+  },
+  timer:{
+    color: "#e5d9cf",
+    fontSize: 54,
+    fontWeight: 'bold'
+  },
+  imagem: {
+    width: 200,
+    height: 200,
+  },
+  actions: {
+    padding: 24,
+    backgroundColor: "#db5226",
+    width: "80%",
+    borderRadius: 32,
+    borderWidth: 2,
+    borderColor: "#f59e0b",
+  }
+})
