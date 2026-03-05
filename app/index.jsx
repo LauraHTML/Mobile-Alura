6f06513a44dbdbd69f63145b1f6b62c61ebdf6ac
LauraHTML <laura.senai03@gmail.com>
2026-03-05 18:56:21 -0300

    feat: iteração sobre a lista de modos de foco
    style: estilização para quando o botão estiver ativo

--- app/index.jsx
+++ app/index.jsx
@@ -1,26 +1,49 @@
+import { useState } from "react";
+
 import { Text, View, StyleSheet, Image, Pressable } from "react-native";
 
+const pomodoro = [
+  {
+    id: 'foco',
+    valorInicial: '25',
+    imagem: require("./tomate.jpg"),
+    display: 'Foco'
+  },
+  {
+    id: 'curto',
+    valorInicial: '5',
+    imagem: require("./curto.jpg"),
+    display: 'Pausa curta'
+  },
+  {
+    id: 'longo',
+    valorInicial: '15',
+    imagem: require("./longo.jpg"),
+    display: 'Pausa longa'
+  }
+]
+
 export default function Index() {
+
+  const [tipoTimer, setTipoTimer] = useState(pomodoro[0])
+
   return (
     <View style={estilos.container}>
-      <Image style={estilos.imagem} source={require("./tomate.jpg")} />
+      <Image style={estilos.imagem} source={tipoTimer.imagem} />
 
       <View style={estilos.actions}>
         {/* tabs */}
-        <View>
-          <Pressable>
-            <Text>Foco</Text>
-          </Pressable>
-
-          <Pressable>
-            <Text>Pausa curta</Text>
-          </Pressable>
+        <View style={estilos.tabs}>
+          {pomodoro.map((timer) => (
+            <Pressable key={timer.id} style={tipoTimer.id === timer.id ? estilos.tabsButtonActive : null} onPress={() => setTipoTimer(timer)}>
+              <Text style={estilos.tabsText}>{timer.display}</Text>
+            </Pressable>
+          ))}
         </View>
 
-        <Pressable>
-            <Text>Pausa longa</Text>
-          </Pressable>
-        <Text style={estilos.timer}>25:00</Text>
+        <Text style={estilos.timer}>
+          {new Date(tipoTimer.valorInicial * 1000).toLocaleTimeString('pt-br', { minute: '2-digit', second: '2-digit' })}
+        </Text>
         <Pressable style={estilos.button}>
           <Text style={estilos.textButton}>Iniciar</Text>
         </Pressable>
@@ -44,7 +67,7 @@ const estilos = StyleSheet.create({
     gap: 40,
   },
   timer: {
-    color: "#e5d9cf",
+    color: "#2b1108",
     fontSize: 54,
     fontWeight: 'bold',
     textAlign: "center"
@@ -78,7 +101,17 @@ const estilos = StyleSheet.create({
     fontSize: 12.5,
   },
   // tabs
-  tabs:{
-    
+  tabs: {
+    flexDirection: "row",
+    gap: 10
+  },
+  tabsText: {
+    padding: 8,
+    color: "#2b1108"
+  },
+  tabsButtonActive: {
+    backgroundColor: "#f59e0b",
+    borderRadius: 8,
+    fontWeight: 'bold',
   }
 })
